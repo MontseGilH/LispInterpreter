d4abd3f79ce2e0f2ee31a1ae6f6c1bb585fd5fb5
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-03-01 08:46:58 -0600

    iniciar lector

--- Lector.java
+++ Lector.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Ultima modificacion: 1/03/2022
  * 
@@ -5,6 +7,59 @@
  * @file Lector.java
  */
 
+
 public class Lector {
-    
+    public ArrayList<String> stringArray(String s) throws Exception{
+        ArrayList<String> operacionEnLista = new ArrayList<String>();
+
+        // verificar que el string sea un enunciado y este en notacion lisp
+        if (s == null || s.length() == 0){
+            throw new Exception("error - enunciado vacio");
+        } else if (s.length()<= 4){
+            throw new Exception("error - enunciado muy corto");
+        } else {
+            if (!String.valueOf(s.charAt(0)).equals("(")){
+                throw new Exception("error - enunciado no inicia con parentesis, notacion lisp");
+            } else if (!String.valueOf(s.charAt(s.length()-1)).equals(")")){
+                throw new Exception("error - enunciado no finaliza con parentesis, notacion lisp");
+            }
+        }
+
+        //revisar cuantos parentesis hay y su lugar
+        String[] caracteres = s.split("");
+        ArrayList<Integer> indexInicioParentesis = new ArrayList<Integer>();
+        ArrayList<Integer> indexFinalParentesis = new ArrayList<Integer>();
+        int i = 0; //lleva el conteo de el indice del parentesis
+
+        for (String f:caracteres){
+            if (f.equals("(")){
+                indexInicioParentesis.add(i);
+            } else if (f.equals(")")){
+                indexFinalParentesis.add(i);
+            }
+            i++;
+        }
+
+
+
+
+        return operacionEnLista;
+    }
+
+    public String stringA(String s){
+        String[] caracteres = s.split("");
+        int i = 0; //lleva el conteo de el indice del parentesis
+        int indiceInicial;
+        for (String f:caracteres){
+            if (f.equals("(")){
+                indiceInicial = i;
+
+            } 
+            i++;
+        }
+        return null;
+
+
+        
+    }
 }
--- LispInterpreter.java
+++ LispInterpreter.java
@@ -4,7 +4,21 @@
  * Clase main del interprete
  * @file LispInterpreter.java
  */
+import java.util.Scanner;
 
 public class LispInterpreter {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        boolean terminar = false;
+        System.out.println("Bienvenid@ al interprete de lisp\n");
+        while (!terminar){
+            System.out.println(">>");
+            String enunciado = scan.nextLine();
+            if (enunciado.equalsIgnoreCase("end")){
+                terminar = true;
+            }
+        }
     
+        scan.close();
+    }
 }
