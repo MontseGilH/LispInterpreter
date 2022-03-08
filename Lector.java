f7ca87a8f585b4e606f3adf22fd4f63d3a273a94
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-03-07 22:38:20 -0600

    revisor expresion

--- Lector.java
+++ Lector.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
+
 /**
- * Ultima modificacion: 1/03/2022
+ * Ultima modificacion: 1/07/2022
  * 
  * Clase que interpreta un string en listas
  * @file Lector.java
@@ -9,57 +10,36 @@ import java.util.ArrayList;
 
 
 public class Lector {
-    public ArrayList<String> stringArray(String s) throws Exception{
-        ArrayList<String> operacionEnLista = new ArrayList<String>();
+    public boolean revisaExpresion(String s){
 
         // verificar que el string sea un enunciado y este en notacion lisp
         if (s == null || s.length() == 0){
-            throw new Exception("error - enunciado vacio");
+           return false;
         } else if (s.length()<= 4){
-            throw new Exception("error - enunciado muy corto");
+            return false;
         } else {
             if (!String.valueOf(s.charAt(0)).equals("(")){
-                throw new Exception("error - enunciado no inicia con parentesis, notacion lisp");
+                return false;
             } else if (!String.valueOf(s.charAt(s.length()-1)).equals(")")){
-                throw new Exception("error - enunciado no finaliza con parentesis, notacion lisp");
+                return false;
             }
         }
 
-        //revisar cuantos parentesis hay y su lugar
+        //revisar cuantos parentesis hay 
         String[] caracteres = s.split("");
-        ArrayList<Integer> indexInicioParentesis = new ArrayList<Integer>();
-        ArrayList<Integer> indexFinalParentesis = new ArrayList<Integer>();
-        int i = 0; //lleva el conteo de el indice del parentesis
+        int inicioP = 0; //lleva el conteo de inicio de parentesis
+        int finalP = 0; //lleva el conteo de final de parentesis
 
         for (String f:caracteres){
             if (f.equals("(")){
-                indexInicioParentesis.add(i);
+                inicioP ++;
             } else if (f.equals(")")){
-                indexFinalParentesis.add(i);
+                finalP++;
             }
-            i++;
         }
+        return inicioP == finalP;
 
-
-
-
-        return operacionEnLista;
     }
 
-    public String stringA(String s){
-        String[] caracteres = s.split("");
-        int i = 0; //lleva el conteo de el indice del parentesis
-        int indiceInicial;
-        for (String f:caracteres){
-            if (f.equals("(")){
-                indiceInicial = i;
-
-            } 
-            i++;
-        }
-        return null;
-
-
-        
-    }
+    
 }
