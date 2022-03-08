02a14fc03322bb52865168f0080e3dcc2c2dbf74
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-03-07 23:01:05 -0600

    probar el metodo que revisa la expresion

--- ClaseTest.java
+++ ClaseTest.java
@@ -5,9 +5,25 @@
  * @file ClaseTest.java
  */
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class ClaseTest {
     
+	
+	/**
+	 * Evalua el metodo de validar sintaxis de una funcion
+	 */
+	@Test
+	void testRevisarExpresion()  {
+		Lector l = new Lector();
+		assertEquals(true,l.revisaExpresion("(+ 2 3)"));
+		assertEquals(true,l.revisaExpresion("(+ 2 3()()() (+ 3 4 3))"));
+		assertEquals(false,l.revisaExpresion("(+ 2 )3)"));
+		assertEquals(false,l.revisaExpresion("+ 2 (3)"));
+		assertEquals(false,l.revisaExpresion("(+ 2 )3"));
+		assertEquals(false,l.revisaExpresion(" "));
+	}
 }
 
 
